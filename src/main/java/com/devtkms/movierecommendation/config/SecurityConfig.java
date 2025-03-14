2bd7515e130cf39768ba577a76437314fb087117
Takumi Suzuki <devtkmszk824@gmail.com>
2025-03-14 15:35:35 +0900

    SecurityConfig.javaを修正

--- src/main/java/com/devtkms/movierecommendation/config/SecurityConfig.java
+++ src/main/java/com/devtkms/movierecommendation/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     CorsConfigurationSource apiConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:3000"
-                ,"https://pattocinema-api.onrender.com","https://pattocinema.onrender.com"));
+                ,"https://pattocinema.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 必要なメソッドを許可
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // 必要なヘッダーを許可
         configuration.setAllowCredentials(true);
