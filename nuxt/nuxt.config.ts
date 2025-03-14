a2d832b378e46612bdbd81315d0ad2bc700f2fa3
Takumi Suzuki <devtkmszk824@gmail.com>
2025-03-14 11:39:27 +0900

    nuxt.config.tsを修正

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,4 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  devServer: {
+    port: Number(process.env.PORT) || 3000  // ✅ PORT を number に変換
+  }
 });
