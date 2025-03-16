990b76d89a9e5ca29468bca293fe5c27567d6e62
Takumi Suzuki <devtkmszk824@gmail.com>
2025-03-17 08:23:18 +0900

    AWS lambda関連を削除

--- src/main/java/com/devtkms/movierecommendation/MovieRecommendationApplication.java
+++ src/main/java/com/devtkms/movierecommendation/MovieRecommendationApplication.java
@@ -1,7 +1,5 @@
 package com.devtkms.movierecommendation;
 
-import com.devtkms.movierecommendation.handler.MovieRecommendationHandler;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -11,8 +9,6 @@ import org.springframework.cache.annotation.EnableCaching;
 public class MovieRecommendationApplication {
     public static void main(String[] args) {
         SpringApplication.run(MovieRecommendationApplication.class, args);
-
-        new MovieRecommendationHandler();
     }
 
 }
