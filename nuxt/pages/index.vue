af6d55cbffe73b07d66ba91028e032300f6a0830
Takumi Suzuki <devtkmszk824@gmail.com>
2025-03-25 18:52:43 +0900

    本番URL変更漏れ

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -228,7 +228,9 @@ const fetchMovies = async () => {
   }
 
   try {
-    const response = await fetch(`http://localhost:8080/api/movies`, {
+    // const response = await fetch(`${config.public.apiBase}/movies`,{
+    // const response = await fetch(`http://localhost:8080/api/movies`, {
+      const response = await fetch(`https://movie-recommendation-uybc.onrender.com/api/movies`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(selectedOptions.value),
