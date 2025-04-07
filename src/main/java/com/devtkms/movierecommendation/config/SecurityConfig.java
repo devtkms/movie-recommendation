f6b13f18f38a71fc9adab7509706c03a7adf9e5a
Takumi Suzuki <devtkmszk824@gmail.com>
2025-04-07 10:49:39 +0900

    Cookieを追加

--- src/main/java/com/devtkms/movierecommendation/config/SecurityConfig.java
+++ src/main/java/com/devtkms/movierecommendation/config/SecurityConfig.java
@@ -103,7 +103,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:3000"
                 ,"https://movireco.onrender.com","https://movireco.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 必要なメソッドを許可
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // 必要なヘッダーを許可
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cookie")); // 必要なヘッダーを許可
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
